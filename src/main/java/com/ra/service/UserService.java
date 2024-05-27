bd6f34b9372d743c18da1380d10b7a7ff1171d56
binh-1892001 <=>
2024-05-27 15:51:31 +0700

    update

--- src/main/java/com/ra/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ra.service;
-
-import com.ra.model.Users;
-
-import java.util.List;
-
-public interface UserService extends IBaseService<Users,String>{
-	List<Users> searchUserByFullName(String keyword);
-}
